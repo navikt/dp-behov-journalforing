44a4737238db12b374c8e7f7cc5fba2db425975f
Celine Dam <celine.ten.dam@nav.no>
2022-03-29 11:00:29 +0200

    Ny eksempelapp
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>

--- src/main/kotlin/no/nav/dagpenger/behov/journalforing/App.kt
+++ src/main/kotlin/no/nav/dagpenger/behov/journalforing/App.kt
@@ -1,41 +1,18 @@
 package no.nav.dagpenger.behov.journalforing
 
-import no.nav.helse.rapids_rivers.JsonMessage
-import no.nav.helse.rapids_rivers.MessageContext
-import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
-import no.nav.helse.rapids_rivers.River
 
 fun main() {
-    val env = System.getenv()
-
-    RapidApplication.create(env).apply {
-        MyCoolApp(this)
-    }.start()
+    App.start()
 }
 
-internal class MyCoolApp(
-    rapidsConnection: RapidsConnection,
-) : River.PacketListener {
+internal object App : RapidsConnection.StatusListener {
+    private val rapidsConnection = RapidApplication.create(Configuration.config)
 
     init {
-        River(rapidsConnection).apply {
-            validate { it.demandValue("@event_name", "my_event") }
-            validate { it.requireKey("a_required_key") }
-            // nested objects can be chained using "."
-            validate { it.requireValue("nested.key", "works_as_well") }
-        }.register(this)
+        rapidsConnection.register(this)
     }
 
-    override fun onError(problems: MessageProblems, context: MessageContext) {
-        /* fordi vi bruker demandValue() på event_name kan vi trygt anta at meldingen
-           er "my_event", og at det er minst én av de ulike require*() som har feilet */
-    }
-
-    override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        println(packet["a_required_key"].asText())
-        // nested objects can be chained using "."
-        println(packet["nested.key"].asText())
-    }
+    fun start() = rapidsConnection.start()
 }
