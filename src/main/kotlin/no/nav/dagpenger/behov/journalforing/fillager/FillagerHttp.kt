7b1d6e1f813cacdb7ae43d43956bdb0bfb4fcad7
André Roaldseth <andre.roaldseth@nav.no>
2022-03-30 14:30:54 +0200

    Ta bort unødvendig JSON-støtte i FillagerHttp

--- src/main/kotlin/no/nav/dagpenger/behov/journalforing/fillager/FillagerHttp.kt
+++ src/main/kotlin/no/nav/dagpenger/behov/journalforing/fillager/FillagerHttp.kt
@@ -3,16 +3,13 @@ package no.nav.dagpenger.behov.journalforing.fillager
 import de.slub.urn.URN
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.http.HttpHeaders
 import no.nav.dagpenger.aad.api.ClientCredentialsClient
 
 class FillagerHttp(engine: HttpClientEngine, private val tokenProvider: ClientCredentialsClient) : Fillager {
-    private val client = HttpClient(engine) {
-        install(JsonFeature) {}
-    }
+    private val client = HttpClient(engine) {}
 
     override suspend fun hentFil(urn: String): String {
         val urn = URN.rfc8141().parse(urn)
