e09ef3eba759378fa8210569444960692ec61f9a
Giao The Cung <giaothe@gmail.com>
2024-06-21 10:11:54 +0200

    Bruke ny behovloser
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- src/main/kotlin/no/nav/dagpenger/behov/journalforing/App.kt
+++ src/main/kotlin/no/nav/dagpenger/behov/journalforing/App.kt
@@ -6,6 +6,7 @@ import no.nav.dagpenger.behov.journalforing.Configuration.mellomlagringTokenProv
 import no.nav.dagpenger.behov.journalforing.fillager.FillagerHttp
 import no.nav.dagpenger.behov.journalforing.journalpost.JournalpostApiHttp
 import no.nav.dagpenger.behov.journalforing.soknad.SoknadHttp
+import no.nav.dagpenger.behov.journalforing.tjenester.GenerellJournalføringBehovløser
 import no.nav.dagpenger.behov.journalforing.tjenester.JournalforingBehovLøser
 import no.nav.dagpenger.behov.journalforing.tjenester.RapporteringJournalføringBehovLøser
 import no.nav.helse.rapids_rivers.RapidApplication
@@ -16,18 +17,26 @@ fun main() {
 }
 
 internal object App : RapidsConnection.StatusListener {
+    private val fillager = FillagerHttp(tokenProvider = mellomlagringTokenProvider)
+    private val journalpostApi = JournalpostApiHttp(tokenProvider = dokarkivTokenProvider)
+
     private val rapidsConnection =
         RapidApplication.create(Configuration.config).also {
             JournalforingBehovLøser(
-                it,
-                FillagerHttp(tokenProvider = mellomlagringTokenProvider),
-                JournalpostApiHttp(tokenProvider = dokarkivTokenProvider),
-                SoknadHttp(tokenProvider = dpSøknadTokenProvider),
+                rapidsConnection = it,
+                fillager = fillager,
+                journalpostApi = journalpostApi,
+                faktahenter = SoknadHttp(tokenProvider = dpSøknadTokenProvider),
             )
             RapporteringJournalføringBehovLøser(
-                it,
-                FillagerHttp(tokenProvider = mellomlagringTokenProvider),
-                JournalpostApiHttp(tokenProvider = dokarkivTokenProvider),
+                rapidsConnection = it,
+                fillager = fillager,
+                journalpostApi = journalpostApi,
+            )
+            GenerellJournalføringBehovløser(
+                rapidsConnection = it,
+                fillager = fillager,
+                journalpostApi = journalpostApi
             )
         }
 
