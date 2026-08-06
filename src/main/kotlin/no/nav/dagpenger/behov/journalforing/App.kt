b8f670e6a494768902623a92cd28304732f9533b
ArselanSultani <arselan.sultani@nav.no>
2026-08-06 14:39:04 +0200

    Fjern JournalforingBehovLøser og SoknadHttp (#242)
    * Fjern JournalforingBehovLøser og SoknadHttp
    
    * Fjern MinidialogJournalføringBehovLøser

--- src/main/kotlin/no/nav/dagpenger/behov/journalforing/App.kt
+++ src/main/kotlin/no/nav/dagpenger/behov/journalforing/App.kt
@@ -2,16 +2,12 @@ package no.nav.dagpenger.behov.journalforing
 
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import no.nav.dagpenger.behov.journalforing.Configuration.dokarkivTokenProvider
-import no.nav.dagpenger.behov.journalforing.Configuration.dpSøknadTokenProvider
 import no.nav.dagpenger.behov.journalforing.Configuration.mellomlagringTokenProvider
 import no.nav.dagpenger.behov.journalforing.fillager.FillagerHttp
 import no.nav.dagpenger.behov.journalforing.journalpost.JournalpostApiHttp
-import no.nav.dagpenger.behov.journalforing.soknad.SoknadHttp
-import no.nav.dagpenger.behov.journalforing.tjenester.JournalforingBehovLøser
 import no.nav.dagpenger.behov.journalforing.tjenester.JournalførEttersendingBehovLøser
 import no.nav.dagpenger.behov.journalforing.tjenester.JournalførSøknadPdfOgVedleggBehovLøser
 import no.nav.dagpenger.behov.journalforing.tjenester.MeldekortJournalføringBehovLøser
-import no.nav.dagpenger.behov.journalforing.tjenester.MinidialogJournalføringBehovLøser
 import no.nav.dagpenger.behov.journalforing.tjenester.RapporteringJournalføringBehovLøser
 import no.nav.dagpenger.behov.journalforing.tjenester.VedtaksbrevJournalføringBehovløser
 import no.nav.helse.rapids_rivers.RapidApplication
@@ -26,12 +22,6 @@ internal object App : RapidsConnection.StatusListener {
 
     private val rapidsConnection =
         RapidApplication.create(Configuration.config).also {
-            JournalforingBehovLøser(
-                rapidsConnection = it,
-                fillager = fillager,
-                journalpostApi = journalpostApi,
-                faktahenter = SoknadHttp(tokenProvider = dpSøknadTokenProvider),
-            )
             JournalførSøknadPdfOgVedleggBehovLøser(
                 rapidsConnection = it,
                 fillager = fillager,
@@ -40,10 +30,6 @@ internal object App : RapidsConnection.StatusListener {
             JournalførEttersendingBehovLøser(
                 rapidsConnection = it,
                 fillager = fillager,
-                journalpostApi = journalpostApi,
-            )
-            MinidialogJournalføringBehovLøser(
-                rapidsConnection = it,
                 journalpostApi = journalpostApi,
             )
             RapporteringJournalføringBehovLøser(
